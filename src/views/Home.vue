31eb6c21fbce06edc1319d21bf61a83d5b4b89e4
dauletisatayev <dake.99.10@gmail.com>
2019-10-15 22:07:15 +0600

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    {{someData}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      someData: 'I am some data from component'
+    }
+  },
+  methods: {
+  },
+  created() {
+  }
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
+</style>
